58a277bf138d82cc5e84ca8ed04f2fb4398c65e2
Firestar311 <slaterandrew0398@gmail.com>
2024-12-08 18:57:05 -0600

    Removed any requirements for the randoms, up to implementor

--- src/main/java/com/stardevllc/random/BiasedRandom.java
+++ src/main/java/com/stardevllc/random/BiasedRandom.java
@@ -1,6 +1,6 @@
 package com.stardevllc.random;
 
-public interface BiasedRandom<T extends Comparable<T>, V> extends StarRandom<T, V> {
+public interface BiasedRandom<T, V> extends StarRandom<T, V> {
     T getBiasMinimum();
     T getBiasMaximum();
     double getBiasThreshold();
